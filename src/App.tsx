54525d53b84a8d05f268ae260a100ef2b569bbb5
cinos81 <cinos81@gmail.com>
2023-11-17 17:25:06 +0900

    xstate chart 통합

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
 import "./App.css";
-import {useState} from "react";
+import {createMachine} from "xstate";
+import { inspect } from "@xstate/inspect";
+import {atomWithMachine} from "jotai-xstate";
+import {useAtom} from "jotai";
+
+inspect()
+
+const lightMachine= createMachine<string>({
+    id: 'light',
+    // 초기 상태
+    initial: 'green',
+    states: {
+        // 상태 1
+        green: {
+            description: '원의 색을 녹색으로 칠한다',
+            // 상태 1의 트랜지션
+            on: {
+                // 액션이 발생했을때 가리킬 상태
+                다음: 'blue'
+            }
+        },
+        // 상태 2
+        blue: {
+            description: '원의 색을 파란색으로 칠한다',
+            // 상태 2의 트랜지션
+            on: {
+                // 액션이 발생했을때 가리킬 상태
+                다음: 'yellow'
+            }
+        },
+        // 상태 3
+        yellow: {
+            description: '원의 색을 노란색으로 칠한다',
+            // 상태 3의 트랜지션
+            on: {
+                // 액션이 발생했을때 가리킬 상태
+                다음: 'green'
+            }
+        }
+    }
+})
+
+const lightMachineAtom = atomWithMachine(lightMachine, {
+    devTools: true
+})
 
 function App() {
-  const [fill, setFill] = useState('blue');
+  const [state, send] = useAtom(lightMachineAtom);
 
   return (
     <svg>
-      <circle onClick={() => setFill('red')} cx="50" cy="50" r="40" fill={fill} />
+      <circle onClick={() => send('다음')} cx="50" cy="50" r="40" fill={state.value as string} />
     </svg>
   );
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("/");
 });
 
 test("원이 표시된다", async ({ page }) => {
@@ -9,7 +9,17 @@ test("원이 표시된다", async ({ page }) => {
   await expect(page.locator("circle")).toBeVisible();
 });
 
-test('클릭하면 색이 바뀐다', async ({ page }) => {
-  await page.locator("circle").click();
-  await expect(page.locator("circle")).toHaveAttribute("fill", "red");
+test('클릭하면 색이 차례차례 바뀐다', async ({ page }) => {
+  const circle = page.locator("circle");
+  await expect(circle).toHaveAttribute("fill", "green");
+  await circle.click();
+  await expect(circle).toHaveAttribute("fill", "blue");
+  await circle.click();
+  await expect(circle).toHaveAttribute("fill", "yellow");
+  await circle.click();
+  await expect(circle).toHaveAttribute("fill", "green");
+  await circle.click();
+  await expect(circle).toHaveAttribute("fill", "blue");
+  await circle.click();
+  await expect(circle).toHaveAttribute("fill", "yellow");
 })
