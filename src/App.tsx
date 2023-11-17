4715b675a455b3dd879df82c0d2195619462a8df
cinos81 <cinos81@gmail.com>
2023-11-17 17:44:27 +0900

    파생상태 정의

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,40 @@ import "./App.css";
 import {createMachine} from "xstate";
 import { inspect } from "@xstate/inspect";
 import {atomWithMachine} from "jotai-xstate";
-import {useAtom} from "jotai";
+import {atom, useAtomValue, useSetAtom} from "jotai";
 
 inspect()
 
 const lightMachine= createMachine<string>({
     id: 'light',
     // 초기 상태
-    initial: 'green',
+    initial: '녹색',
     states: {
         // 상태 1
-        green: {
+        녹색: {
             description: '원의 색을 녹색으로 칠한다',
             // 상태 1의 트랜지션
             on: {
                 // 액션이 발생했을때 가리킬 상태
-                다음: 'blue'
+                다음: '파란색'
             }
         },
         // 상태 2
-        blue: {
+        파란색: {
             description: '원의 색을 파란색으로 칠한다',
             // 상태 2의 트랜지션
             on: {
                 // 액션이 발생했을때 가리킬 상태
-                다음: 'yellow'
+                다음: '노란색'
             }
         },
         // 상태 3
-        yellow: {
+        노란색: {
             description: '원의 색을 노란색으로 칠한다',
             // 상태 3의 트랜지션
             on: {
                 // 액션이 발생했을때 가리킬 상태
-                다음: 'green'
+                다음: '녹색'
             }
         }
     }
@@ -45,12 +45,27 @@ const lightMachineAtom = atomWithMachine(lightMachine, {
     devTools: true
 })
 
+const fillAtom = atom((get) => {
+    const state = get(lightMachineAtom).value;
+
+    if (state === '녹색') {
+        return 'green'
+    }
+
+    if (state === '파란색') {
+        return 'blue'
+    }
+
+    return 'yellow'
+})
+
 function App() {
-  const [state, send] = useAtom(lightMachineAtom);
+  const fill = useAtomValue(fillAtom)
+  const send = useSetAtom(lightMachineAtom);
 
   return (
     <svg>
-      <circle onClick={() => send('다음')} cx="50" cy="50" r="40" fill={state.value as string} />
+      <circle onClick={() => send('다음')} cx="50" cy="50" r="40" fill={fill} />
     </svg>
   );
 }
